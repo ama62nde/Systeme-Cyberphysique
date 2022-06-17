060c74b4564786d147bbf25cc2ce38b87d538054
maximelonguepe <longuepemaxime73@gmail.com>
2022-06-17 11:35:05 +0200

    ajout communication GPS

--- communicationArduinoRaspberry/SerialPassthrough.ino
+++ communicationArduinoRaspberry/SerialPassthrough.ino
@@ -1,4 +1,3 @@
-
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
@@ -10,6 +9,17 @@ void loop() {
   }
 
   if (Serial2.available()) {     // If anything comes in Serial1 (pins 0 & 1)
-    Serial.write(Serial2.read());   // read it and send it out Serial (USB)
+    String trame=Serial2.readString();
+   // Serial.write(Serial2.read());// read it and send it out Serial (USB)
+    int indexx = trame.indexOf('x');
+    int indexy = trame.indexOf('y');
+    int indexz = trame.indexOf('z');
+    int indexf = trame.indexOf('f');
+    float x=trame.substring(indexx+1,indexy-1).toFloat();
+    float y=trame.substring(indexy+1,indexz-1).toFloat();
+    float z=trame.substring(indexz+1,indexf-1).toFloat();
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println(z);
   }
 }
--- marvelmindposition/communicationPositionMarvelMind.py
+++ marvelmindposition/communicationPositionMarvelMind.py
@@ -3,25 +3,31 @@ from time import sleep
 import sys
 import serial
 import time
+
+
 def main():
     hedge = MarvelmindHedge(tty="/dev/ttyACM0", adr=None, debug=False)  # create MarvelmindHedge thread
 
     if (len(sys.argv) > 1):
         hedge.tty = sys.argv[1]
-    with serial.Serial("/dev/ttyS0",9600,timeout=.1) as arduino :
+    with serial.Serial("/dev/ttyS0", 9600,
+                       timeout=.1) as arduino:  # on tente de se connecter à l'arduino via le port ttyS0
         time.sleep(0.1)
-        if arduino.isOpen():
+        if arduino.isOpen():  # si il est ouvert on commence le thread marvelmind
             hedge.start()  # start thread
             while True:
                 try:
                     hedge.dataEvent.wait(1)
                     hedge.dataEvent.clear()
 
-                    if (hedge.positionUpdated):
-                        test=""
-                        test=test+"x"+str(hedge.position()[1])+"y"+str(hedge.position()[2])+"z"+str(hedge.position()[3])+"f"
-                        test=test+"\n"
+                    if (hedge.positionUpdated):  # ici on va envoyer la position si la position a changer
+
+                        test = ""
+                        test = test + "x" + str(hedge.position()[1]) + "y" + str(hedge.position()[2]) + "z" + str(
+                            hedge.position()[3]) + "f"
+                        test = test + "\n"
                         print(test)
+                        # on va envoyer une chaine sous le format x2.559y-1.428z-0.665f et l'arduino va split cette chaine
                         arduino.write(test.encode())
                         hedge.print_position()
 
